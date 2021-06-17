2dce1c534fd7c69db688fbda3a2ad1a52201a624
unknown <bnsviv@gmail.com>
2021-06-17 11:08:11 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+Inherit from SyntaxError
+importance: 5
+Create a class FormatError that inherits from the built-in 
+SyntaxError class.
+
+It should support message, name and stack properties.
+
+Usage example:
+
+let err = new FormatError("formatting error");
+
+alert( err.message ); // formatting error
+alert( err.name ); // FormatError
+alert( err.stack ); // stack
+
+alert( err instanceof FormatError ); // true
+alert( err instanceof SyntaxError ); // true (because inherits from SyntaxError)
+
+class FormatError extends SyntaxError {
+	constructor(message) {
+		super(message);
+		this.name = this.constructor.name;
+	}
+}
+
+let err = new FormatError("formatting error");
+
+alert( err.message ); // formatting error
+alert( err.name ); // FormatError
+alert( err.stack ); // stack
+
+alert( err instanceof SyntaxError ); // true
+
